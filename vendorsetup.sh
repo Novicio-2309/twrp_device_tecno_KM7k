06b4b735a671d939c8d518b8856333b83d4d0aa1
Novicio-2309 <noviciogaming1997@gmail.com>
2026-03-09 21:17:51 +0800

    Update vendorsetup.sh for TWRP

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-add_lunch_combo twrp_KM7k-eng
-add_lunch_combo twrp_KM7k-userdebug
-add_lunch_combo twrp_KM7k-user
+# add_lunch_combo twrp_KM7k-eng
+# add_lunch_combo twrp_KM7k-userdebug
+# add_lunch_combo twrp_KM7k-user
